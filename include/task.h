88e32327e975ddde97c390bc5b6c1f8e7d9d239e
Yuhui.Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2020-02-18 22:03:54 -0800

    version bump to v10.3.1 (#16)
    * Verion bump from 10.3.0 to 10.3.1.
    * version bump in task.h
    * change history for 10.3.1.

--- include/task.h
+++ include/task.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS Kernel V10.3.0
+ * FreeRTOS Kernel V10.3.1
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -43,10 +43,10 @@ extern "C" {
  * MACROS AND DEFINITIONS
  *----------------------------------------------------------*/
 
-#define tskKERNEL_VERSION_NUMBER "V10.3.0"
+#define tskKERNEL_VERSION_NUMBER "V10.3.1"
 #define tskKERNEL_VERSION_MAJOR 10
 #define tskKERNEL_VERSION_MINOR 3
-#define tskKERNEL_VERSION_BUILD 0
+#define tskKERNEL_VERSION_BUILD 1
 
 /* MPU region parameters passed in ulParameters
  * of MemoryRegion_t struct. */
