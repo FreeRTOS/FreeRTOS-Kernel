09bff845b0c5d1111d66a95824bc157eb0ae3dbe
Gaurav Aggarwal <aggarg@amazon.com>
2020-04-14 01:28:31 -0700

    Add definition of portDONT_DISCARD to ARMv7-M ports
    Enabling Link Time Optimization (LTO) causes some of the functions used
    in assembly to be incorrectly stripped off, resulting in linker error.
    To avoid this, these functions are marked with portDONT_DISCARD macro,
    definition of which is port specific. This commit adds the definition
    of portDONT_DISCARD for ARMv7-M ports.
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- portable/GCC/ARM_CM4F/portmacro.h
+++ portable/GCC/ARM_CM4F/portmacro.h
@@ -73,6 +73,7 @@ typedef unsigned long UBaseType_t;
 #define portSTACK_GROWTH			( -1 )
 #define portTICK_PERIOD_MS			( ( TickType_t ) 1000 / configTICK_RATE_HZ )
 #define portBYTE_ALIGNMENT			8
+#define portDONT_DISCARD			__attribute__(( used ))
 /*-----------------------------------------------------------*/
 
 /* Scheduler utilities. */
