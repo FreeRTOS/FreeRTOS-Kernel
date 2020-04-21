7eafef28c0d459b5cd874ae7f8fd975e253f6ba6
David Chalco <david@chalco.io>
2020-04-21 16:50:45 -0700

    Add descriptive comment for portTIMER_CALLBACK_ATTRIBUTE and move to same line as decorated code

--- timers.c
+++ timers.c
@@ -69,15 +69,17 @@ defining trmTIMER_SERVICE_TASK_NAME in FreeRTOSConfig.h. */
 #define tmrSTATUS_IS_STATICALLY_ALLOCATED	( ( uint8_t ) 0x02 )
 #define tmrSTATUS_IS_AUTORELOAD				( ( uint8_t ) 0x04 )
 
-/* The definition of the timers themselves. */
+/* The definition of the timers themselves. 
+portTIMER_CALLBACK_ATTRIBUTE can be used to apply an __attribute__ to timer callbacks and timer
+callback function pointers. In some ports, this attribute enables the compiler to resolve function pointers in static analysis. 
+portTIMER_CALLBACK_ATTRIBUTE defaults as white space unless it is defined by the user. */
 typedef struct tmrTimerControl /* The old naming convention is used to prevent breaking kernel aware debuggers. */
 {
 	const char				*pcTimerName;		/*<< Text name.  This is not used by the kernel, it is included simply to make debugging easier. */ /*lint !e971 Unqualified char types are allowed for strings and single characters only. */
 	ListItem_t				xTimerListItem;		/*<< Standard linked list item as used by all kernel features for event management. */
 	TickType_t				xTimerPeriodInTicks;/*<< How quickly and often the timer expires. */
 	void 					*pvTimerID;			/*<< An ID to identify the timer.  This allows the timer to be identified when the same callback is used for multiple timers. */
-	portTIMER_CALLBACK_ATTRIBUTE
-	TimerCallbackFunction_t	pxCallbackFunction;	/*<< The function that will be called when the timer expires. */
+	portTIMER_CALLBACK_ATTRIBUTE TimerCallbackFunction_t	pxCallbackFunction;	/*<< The function that will be called when the timer expires. */
 	#if( configUSE_TRACE_FACILITY == 1 )
 		UBaseType_t			uxTimerNumber;		/*<< An ID assigned by trace tools such as FreeRTOS+Trace */
 	#endif
@@ -102,8 +104,7 @@ typedef struct tmrTimerParameters
 
 typedef struct tmrCallbackParameters
 {
-	portTIMER_CALLBACK_ATTRIBUTE
-	PendedFunction_t	pxCallbackFunction;	/* << The callback function to execute. */
+	portTIMER_CALLBACK_ATTRIBUTE PendedFunction_t	pxCallbackFunction;	/* << The callback function to execute. */
 	void *pvParameter1;						/* << The value that will be used as the callback functions first parameter. */
 	uint32_t ulParameter2;					/* << The value that will be used as the callback functions second parameter. */
 } CallbackParameters_t;
