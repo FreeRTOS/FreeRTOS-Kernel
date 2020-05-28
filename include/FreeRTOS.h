b2bbdc9719e6624f839798de5c46b914a292f1d2
syntroniks <syntroniks@gmail.com>
2020-05-28 07:37:50 -0500

    updates FreeRTOS.h to handle new usages of task notify

--- include/FreeRTOS.h
+++ include/FreeRTOS.h
@@ -637,7 +637,7 @@ hold explicit before calling the code. */
 #endif
 
 #ifndef traceTASK_NOTIFY_TAKE
-	#define traceTASK_NOTIFY_TAKE()
+	#define traceTASK_NOTIFY_TAKE( uxIndexToWait )
 #endif
 
 #ifndef traceTASK_NOTIFY_WAIT_BLOCK
@@ -645,7 +645,7 @@ hold explicit before calling the code. */
 #endif
 
 #ifndef traceTASK_NOTIFY_WAIT
-	#define traceTASK_NOTIFY_WAIT()
+	#define traceTASK_NOTIFY_WAIT( uxIndexToWait )
 #endif
 
 #ifndef traceTASK_NOTIFY
