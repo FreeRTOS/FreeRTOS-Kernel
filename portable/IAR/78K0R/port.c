54ee4d62bdc6704ea75ad7ffcc23c00e5ed20842
Alfred Gedeon <gedeonag@amazon.com>
2020-08-11 21:56:04 -0700

    Style: Revert more uncrustified files

--- portable/IAR/78K0R/port.c
+++ portable/IAR/78K0R/port.c
@@ -33,8 +33,8 @@
 #include "task.h"
 
 /* The critical nesting value is initialised to a non zero value to ensure
- * interrupts don't accidentally become enabled before the scheduler is started. */
-#define portINITIAL_CRITICAL_NESTING    ( ( uint16_t ) 10 )
+interrupts don't accidentally become enabled before the scheduler is started. */
+#define portINITIAL_CRITICAL_NESTING  (( uint16_t ) 10)
 
 /* Initial PSW value allocated to a newly created task.
  *   1100011000000000
@@ -47,25 +47,25 @@
  *   |--------------------- Zero Flag set
  *   ---------------------- Global Interrupt Flag set (enabled)
  */
-#define portPSW                         ( 0xc6UL )
+#define portPSW		  (0xc6UL)
 
 /* We require the address of the pxCurrentTCB variable, but don't want to know
- * any details of its type. */
+any details of its type. */
 typedef void TCB_t;
 extern volatile TCB_t * volatile pxCurrentTCB;
 
 /* Most ports implement critical sections by placing the interrupt flags on
- * the stack before disabling interrupts.  Exiting the critical section is then
- * simply a case of popping the flags from the stack.  As 78K0 IAR does not use
- * a frame pointer this cannot be done as modifying the stack will clobber all
- * the stack variables.  Instead each task maintains a count of the critical
- * section nesting depth.  Each time a critical section is entered the count is
- * incremented.  Each time a critical section is left the count is decremented -
- * with interrupts only being re-enabled if the count is zero.
- *
- * usCriticalNesting will get set to zero when the scheduler starts, but must
- * not be initialised to zero as this will cause problems during the startup
- * sequence. */
+the stack before disabling interrupts.  Exiting the critical section is then
+simply a case of popping the flags from the stack.  As 78K0 IAR does not use
+a frame pointer this cannot be done as modifying the stack will clobber all
+the stack variables.  Instead each task maintains a count of the critical
+section nesting depth.  Each time a critical section is entered the count is
+incremented.  Each time a critical section is left the count is decremented -
+with interrupts only being re-enabled if the count is zero.
+
+usCriticalNesting will get set to zero when the scheduler starts, but must
+not be initialised to zero as this will cause problems during the startup
+sequence. */
 volatile uint16_t usCriticalNesting = portINITIAL_CRITICAL_NESTING;
 /*-----------------------------------------------------------*/
 
@@ -81,146 +81,145 @@ static void prvSetupTimerInterrupt( void );
  *
  * See the header file portable.h.
  */
-StackType_t * pxPortInitialiseStack( StackType_t * pxTopOfStack,
-                                     TaskFunction_t pxCode,
-                                     void * pvParameters )
+StackType_t *pxPortInitialiseStack( StackType_t *pxTopOfStack, TaskFunction_t pxCode, void *pvParameters )
 {
-    uint32_t * pulLocal;
+uint32_t *pulLocal;
 
-    #if configMEMORY_MODE == 1
-        {
-            /* Parameters are passed in on the stack, and written using a 32bit value
-             * hence a space is left for the second two bytes. */
-            pxTopOfStack--;
+	#if configMEMORY_MODE == 1
+	{
+		/* Parameters are passed in on the stack, and written using a 32bit value
+		hence a space is left for the second two bytes. */
+		pxTopOfStack--;
 
-            /* Write in the parameter value. */
-            pulLocal = ( uint32_t * ) pxTopOfStack;
-            *pulLocal = ( uint32_t ) pvParameters;
-            pxTopOfStack--;
+		/* Write in the parameter value. */
+		pulLocal =  ( uint32_t * ) pxTopOfStack;
+		*pulLocal = ( uint32_t ) pvParameters;
+		pxTopOfStack--;
 
-            /* These values are just spacers.  The return address of the function
-             * would normally be written here. */
-            *pxTopOfStack = ( StackType_t ) 0xcdcd;
-            pxTopOfStack--;
-            *pxTopOfStack = ( StackType_t ) 0xcdcd;
-            pxTopOfStack--;
+		/* These values are just spacers.  The return address of the function
+		would normally be written here. */
+		*pxTopOfStack = ( StackType_t ) 0xcdcd;
+		pxTopOfStack--;
+		*pxTopOfStack = ( StackType_t ) 0xcdcd;
+		pxTopOfStack--;
 
-            /* The start address / PSW value is also written in as a 32bit value,
-             * so leave a space for the second two bytes. */
-            pxTopOfStack--;
+		/* The start address / PSW value is also written in as a 32bit value,
+		so leave a space for the second two bytes. */
+		pxTopOfStack--;
+	
+		/* Task function start address combined with the PSW. */
+		pulLocal = ( uint32_t * ) pxTopOfStack;
+		*pulLocal = ( ( ( uint32_t ) pxCode ) | ( portPSW << 24UL ) );
+		pxTopOfStack--;
 
-            /* Task function start address combined with the PSW. */
-            pulLocal = ( uint32_t * ) pxTopOfStack;
-            *pulLocal = ( ( ( uint32_t ) pxCode ) | ( portPSW << 24UL ) );
-            pxTopOfStack--;
+		/* An initial value for the AX register. */
+		*pxTopOfStack = ( StackType_t ) 0x1111;
+		pxTopOfStack--;
+	}
+	#else
+	{
+		/* Task function address is written to the stack first.  As it is
+		written as a 32bit value a space is left on the stack for the second
+		two bytes. */
+		pxTopOfStack--;
 
-            /* An initial value for the AX register. */
-            *pxTopOfStack = ( StackType_t ) 0x1111;
-            pxTopOfStack--;
-        }
-    #else /* if configMEMORY_MODE == 1 */
-        {
-            /* Task function address is written to the stack first.  As it is
-             * written as a 32bit value a space is left on the stack for the second
-             * two bytes. */
-            pxTopOfStack--;
+		/* Task function start address combined with the PSW. */
+		pulLocal = ( uint32_t * ) pxTopOfStack;
+		*pulLocal = ( ( ( uint32_t ) pxCode ) | ( portPSW << 24UL ) );
+		pxTopOfStack--;
 
-            /* Task function start address combined with the PSW. */
-            pulLocal = ( uint32_t * ) pxTopOfStack;
-            *pulLocal = ( ( ( uint32_t ) pxCode ) | ( portPSW << 24UL ) );
-            pxTopOfStack--;
+		/* The parameter is passed in AX. */
+		*pxTopOfStack = ( StackType_t ) pvParameters;
+		pxTopOfStack--;
+	}
+	#endif
 
-            /* The parameter is passed in AX. */
-            *pxTopOfStack = ( StackType_t ) pvParameters;
-            pxTopOfStack--;
-        }
-    #endif /* if configMEMORY_MODE == 1 */
+	/* An initial value for the HL register. */
+	*pxTopOfStack = ( StackType_t ) 0x2222;
+	pxTopOfStack--;
 
-    /* An initial value for the HL register. */
-    *pxTopOfStack = ( StackType_t ) 0x2222;
-    pxTopOfStack--;
+	/* CS and ES registers. */
+	*pxTopOfStack = ( StackType_t ) 0x0F00;
+	pxTopOfStack--;
 
-    /* CS and ES registers. */
-    *pxTopOfStack = ( StackType_t ) 0x0F00;
-    pxTopOfStack--;
+	/* Finally the remaining general purpose registers DE and BC */
+	*pxTopOfStack = ( StackType_t ) 0xDEDE;
+	pxTopOfStack--;
+	*pxTopOfStack = ( StackType_t ) 0xBCBC;
+	pxTopOfStack--;
 
-    /* Finally the remaining general purpose registers DE and BC */
-    *pxTopOfStack = ( StackType_t ) 0xDEDE;
-    pxTopOfStack--;
-    *pxTopOfStack = ( StackType_t ) 0xBCBC;
-    pxTopOfStack--;
+	/* Finally the critical section nesting count is set to zero when the task
+	first starts. */
+	*pxTopOfStack = ( StackType_t ) portNO_CRITICAL_SECTION_NESTING;	
 
-    /* Finally the critical section nesting count is set to zero when the task
-     * first starts. */
-    *pxTopOfStack = ( StackType_t ) portNO_CRITICAL_SECTION_NESTING;
-
-    /* Return a pointer to the top of the stack we have generated so this can
-     * be stored in the task control block for the task. */
-    return pxTopOfStack;
+	/* Return a pointer to the top of the stack we have generated so this can
+	be stored in the task control block for the task. */
+	return pxTopOfStack;
 }
 /*-----------------------------------------------------------*/
 
 BaseType_t xPortStartScheduler( void )
 {
-    /* Setup the hardware to generate the tick.  Interrupts are disabled when
-     * this function is called. */
-    prvSetupTimerInterrupt();
+	/* Setup the hardware to generate the tick.  Interrupts are disabled when
+	this function is called. */
+	prvSetupTimerInterrupt();
 
-    /* Restore the context of the first task that is going to run. */
-    vPortStart();
+	/* Restore the context of the first task that is going to run. */
+	vPortStart();
 
-    /* Should not get here as the tasks are now running! */
-    return pdTRUE;
+	/* Should not get here as the tasks are now running! */
+	return pdTRUE;
 }
 /*-----------------------------------------------------------*/
 
 void vPortEndScheduler( void )
 {
-    /* It is unlikely that the 78K0R port will get stopped.  If required simply
-     * disable the tick interrupt here. */
+	/* It is unlikely that the 78K0R port will get stopped.  If required simply
+	disable the tick interrupt here. */
 }
 /*-----------------------------------------------------------*/
 
 static void prvSetupTimerInterrupt( void )
 {
-    /* Setup channel 5 of the TAU to generate the tick interrupt. */
+	/* Setup channel 5 of the TAU to generate the tick interrupt. */
 
-    /* First the Timer Array Unit has to be enabled. */
-    TAU0EN = 1;
+	/* First the Timer Array Unit has to be enabled. */
+	TAU0EN = 1;
 
-    /* To configure the Timer Array Unit all Channels have to first be stopped. */
-    TT0 = 0xff;
+	/* To configure the Timer Array Unit all Channels have to first be stopped. */
+	TT0 = 0xff;
 
-    /* Interrupt of Timer Array Unit Channel 5 is disabled to set the interrupt
-     * priority. */
-    TMMK05 = 1;
+	/* Interrupt of Timer Array Unit Channel 5 is disabled to set the interrupt
+	priority. */
+	TMMK05 = 1;
 
-    /* Clear Timer Array Unit Channel 5 interrupt flag. */
-    TMIF05 = 0;
+	/* Clear Timer Array Unit Channel 5 interrupt flag. */	
+	TMIF05 = 0;
 
-    /* Set Timer Array Unit Channel 5 interrupt priority */
-    TMPR005 = 0;
-    TMPR105 = 0;
+	/* Set Timer Array Unit Channel 5 interrupt priority */
+	TMPR005 = 0;
+	TMPR105 = 0;
 
-    /* Set Timer Array Unit Channel 5 Mode as interval timer. */
-    TMR05 = 0x0000;
+	/* Set Timer Array Unit Channel 5 Mode as interval timer. */
+	TMR05 = 0x0000;
 
-    /* Set the compare match value according to the tick rate we want. */
-    TDR05 = ( TickType_t ) ( configCPU_CLOCK_HZ / configTICK_RATE_HZ );
+	/* Set the compare match value according to the tick rate we want. */
+	TDR05 = ( TickType_t ) ( configCPU_CLOCK_HZ / configTICK_RATE_HZ );
 
-    /* Set Timer Array Unit Channel 5 output mode */
-    TOM0 &= ~0x0020;
+	/* Set Timer Array Unit Channel 5 output mode */
+	TOM0 &= ~0x0020;
 
-    /* Set Timer Array Unit Channel 5 output level */
-    TOL0 &= ~0x0020;
+	/* Set Timer Array Unit Channel 5 output level */	
+	TOL0 &= ~0x0020;
 
-    /* Set Timer Array Unit Channel 5 output enable */
-    TOE0 &= ~0x0020;
+	/* Set Timer Array Unit Channel 5 output enable */	
+	TOE0 &= ~0x0020;
 
-    /* Interrupt of Timer Array Unit Channel 5 enabled */
-    TMMK05 = 0;
+	/* Interrupt of Timer Array Unit Channel 5 enabled */
+	TMMK05 = 0;
 
-    /* Start Timer Array Unit Channel 5.*/
-    TS0 |= 0x0020;
+	/* Start Timer Array Unit Channel 5.*/
+	TS0 |= 0x0020;
 }
 /*-----------------------------------------------------------*/
+
