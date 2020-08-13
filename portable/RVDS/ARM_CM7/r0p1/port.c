46c1bd6ce28fe9f221ced9c513780a45cbac17c8
Alfred Gedeon <gedeonag@amazon.com>
2020-08-13 12:16:28 -0700

    Style: add uncrustify disable parsing for asm

--- portable/RVDS/ARM_CM7/r0p1/port.c
+++ portable/RVDS/ARM_CM7/r0p1/port.c
@@ -236,9 +236,10 @@ __asm void vPortSVCHandler( void )
 {
     PRESERVE8
 
+/* *INDENT-OFF* */
     /* Get the location of the current TCB. */
-    ldr r3, = pxCurrentTCB
-              ldr r1, [ r3 ]
+    ldr r3, =pxCurrentTCB
+    ldr r1, [ r3 ]
     ldr r0, [ r1 ]
     /* Pop the core registers. */
     ldmia r0 !, {
@@ -246,19 +247,21 @@ __asm void vPortSVCHandler( void )
     }
     msr psp, r0
     isb
-    mov r0, # 0
+    mov r0, #0
     msr basepri, r0
     bx r14
+/* *INDENT-ON* */
 }
 /*-----------------------------------------------------------*/
 
 __asm void prvStartFirstTask( void )
 {
+/* *INDENT-OFF* */
     PRESERVE8
 
     /* Use the NVIC offset register to locate the stack. */
-    ldr r0, = 0xE000ED08
-              ldr r0, [ r0 ]
+    ldr r0, =0xE000ED08
+    ldr r0, [ r0 ]
     ldr r0, [ r0 ]
     /* Set the msp back to the start of the stack. */
     msr msp, r0
@@ -267,7 +270,7 @@ __asm void prvStartFirstTask( void )
      * before the scheduler was started - which would otherwise result in the
      * unnecessary leaving of space in the SVC stack for lazy saving of FPU
      * registers. */
-    mov r0, # 0
+    mov r0, #0
     msr control, r0
     /* Globally enable interrupts. */
     cpsie i
@@ -277,23 +280,26 @@ __asm void prvStartFirstTask( void )
     /* Call SVC to start the first task. */
         svc 0
     nop
-        nop
+    nop
+/* *INDENT-ON* */
 }
 /*-----------------------------------------------------------*/
 
 __asm void prvEnableVFP( void )
 {
+/* *INDENT-OFF* */
     PRESERVE8
 
     /* The FPU enable bits are in the CPACR. */
-    ldr.w r0, = 0xE000ED88
-                ldr r1, [ r0 ]
+    ldr.w r0, =0xE000ED88
+    ldr r1, [ r0 ]
 
     /* Enable CP10 and CP11 coprocessors, then save back. */
-    orr r1, r1, # ( 0xf << 20 )
+    orr r1, r1, #( 0xf << 20 )
     str r1, [ r0 ]
     bx r14
     nop
+/* *INDENT-ON* */
 }
 /*-----------------------------------------------------------*/
 
@@ -438,16 +444,17 @@ __asm void xPortPendSVHandler( void )
     extern pxCurrentTCB;
     extern vTaskSwitchContext;
 
+/* *INDENT-OFF* */
     PRESERVE8
 
     mrs r0, psp
     isb
     /* Get the location of the current TCB. */
-    ldr r3, = pxCurrentTCB
-              ldr r2, [ r3 ]
+    ldr r3, =pxCurrentTCB
+    ldr r2, [ r3 ]
 
     /* Is the task using the FPU context?  If so, push high vfp registers. */
-    tst r14, # 0x10
+    tst r14, #0x10
     it eq
     vstmdbeq r0 !, {
         s16 - s31
@@ -464,14 +471,14 @@ __asm void xPortPendSVHandler( void )
     stmdb sp !, {
         r0, r3
     }
-    mov r0, # configMAX_SYSCALL_INTERRUPT_PRIORITY
+    mov r0, #configMAX_SYSCALL_INTERRUPT_PRIORITY
     cpsid i
     msr basepri, r0
     dsb
     isb
     cpsie i
     bl vTaskSwitchContext
-    mov r0, # 0
+    mov r0, #0
     msr basepri, r0
     ldmia sp !, {
         r0, r3
@@ -488,14 +495,14 @@ __asm void xPortPendSVHandler( void )
 
     /* Is the task using the FPU context?  If so, pop the high vfp registers
      * too. */
-    tst r14, # 0x10
+    tst r14, #0x10
     it eq
     vldmiaeq r0 !, {
         s16 - s31
     }
 
     msr psp, r0
-        isb
+    isb
     #ifdef WORKAROUND_PMU_CM001 /* XMC4000 specific errata */
         #if WORKAROUND_PMU_CM001 == 1
             push {
@@ -509,6 +516,7 @@ __asm void xPortPendSVHandler( void )
     #endif
 
     bx r14
+/* *INDENT-ON* */
 }
 /*-----------------------------------------------------------*/
 
@@ -736,10 +744,12 @@ void xPortSysTickHandler( void )
 
 __asm uint32_t vPortGetIPSR( void )
 {
+/* *INDENT-OFF* */
     PRESERVE8
 
     mrs r0, ipsr
     bx r14
+/* *INDENT-ON* */
 }
 /*-----------------------------------------------------------*/
 
