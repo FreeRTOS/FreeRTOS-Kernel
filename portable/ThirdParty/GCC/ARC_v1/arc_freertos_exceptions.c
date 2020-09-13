6fdeefe5eb3fd738add77058acc26ac205020538
RichardBarry <ribarry@amazon.com>
2020-09-13 09:21:02 -0700

    Sets the version number to 10.4.0 in assembly files.  The assembly files were missed when the other source files had their version numbers updated.

--- portable/ThirdParty/GCC/ARC_v1/arc_freertos_exceptions.c
+++ portable/ThirdParty/GCC/ARC_v1/arc_freertos_exceptions.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS Kernel V10.2.1
+ * FreeRTOS Kernel V10.4.0
  * Copyright (C) 2020 Synopsys, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
