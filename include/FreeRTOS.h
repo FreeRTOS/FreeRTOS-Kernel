24221dfed84b18bfc23b3ccfb252bab3d2f84acf
Paul Bartell <pbartell@amazon.com>
2021-04-07 20:50:00 -0700

    Remove warning for configMAX_CO_ROUTINE_PRIORITIES

--- include/FreeRTOS.h
+++ include/FreeRTOS.h
@@ -199,11 +199,7 @@
 #endif
 
 #if ( defined( configUSE_CO_ROUTINES ) && configUSE_CO_ROUTINES != 0 )
-    #warning Co-routines have been deprecated and removed from FreeRTOS-Kernel versions after the V10.4.3 release. You can view previous versions of the FreeRTOS Kernel at github.com/freertos/freertos-kernel/tree/V10.4.3 .
-#endif
-
-#if defined( configMAX_CO_ROUTINE_PRIORITIES )
-    #warning configMAX_CO_ROUTINE_PRIORITIES is deprecated and no longer used after FreeRTOS V10.4.3.
+    #warning Co-routines have been removed from FreeRTOS-Kernel versions released after V10.4.3. You can view previous versions of the FreeRTOS Kernel at github.com/freertos/freertos-kernel/tree/V10.4.3 .
 #endif
 
 #ifndef configUSE_DAEMON_TASK_STARTUP_HOOK
