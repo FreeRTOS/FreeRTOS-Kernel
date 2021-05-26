573c9d5bb6378490c052cc57b95578959a8a6e71
Paul Bartell <pbartell@amazon.com>
2021-05-26 16:16:27 -0700

    Add SPDX-License-Identifier: MIT to MIT licensed files.

--- portable/ThirdParty/CDK/T-HEAD_CK802/portasm.S
+++ portable/ThirdParty/CDK/T-HEAD_CK802/portasm.S
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2017 C-SKY Microsystems Co., Ltd. All rights reserved.
  *
+ * SPDX-License-Identifier: MIT
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
