b4a7a046579c5a9b358fcce582c5e64d2af8956a
Joseph Julicher <jjulicher@mac.com>
2021-05-28 15:48:53 -0700

    Add history.txt for the 10.4.4 release (#336)
    * updated history.txt for 10.4.4
    
    * Update the release date in History.txt
    
    * added link to SMP branch to History.txt
    
    * Added comment explaining the + in the version string
    
    * corrected typos in the + comment
    
    Co-authored-by: alfred gedeon <28123637+alfred2g@users.noreply.github.com>
    Co-authored-by: Paul Bartell <pbartell@amazon.com>

--- include/task.h
+++ include/task.h
@@ -46,10 +46,17 @@
 * MACROS AND DEFINITIONS
 *----------------------------------------------------------*/
 
-#define tskKERNEL_VERSION_NUMBER       "V10.4.999"
+/*
+ * If tskKERNEL_VERSION_NUMBER ends with + it represents the version in development
+ * after the numbered release.
+ *
+ * The tskKERNEL_VERSION_MAJOR, tskKERNEL_VERSION_MINOR, tskKERNEL_VERSION_BUILD
+ * values will reflect the last released version number.
+ */ 
+#define tskKERNEL_VERSION_NUMBER       "V10.4.3+"
 #define tskKERNEL_VERSION_MAJOR        10
 #define tskKERNEL_VERSION_MINOR        4
-#define tskKERNEL_VERSION_BUILD        999
+#define tskKERNEL_VERSION_BUILD        3
 
 /* MPU region parameters passed in ulParameters
  * of MemoryRegion_t struct. */
