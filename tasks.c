8a50527bff5c3743df14155d9aeb7104d7965f78
Laukik Hase <laukik.hase@espressif.com>
2021-11-10 12:34:38 +0530

    bugfix: Initialize uxTaskNumber at task initialization

--- tasks.c
+++ tasks.c
@@ -851,6 +851,13 @@ static void prvInitialiseNewTask( TaskFunction_t pxTaskCode,
         }
     #endif /* tskSET_NEW_STACKS_TO_KNOWN_VALUE */
 
+    #if ( configUSE_TRACE_FACILITY == 1 )
+        {
+            /* Zero the uxTaskNumber TCB member to avoid random value from dynamically allocated TCBs */
+            pxNewTCB->uxTaskNumber = 0;
+        }
+    #endif  /* configUSE_TRACE_FACILITY */
+
     /* Calculate the top of stack address.  This depends on whether the stack
      * grows from high memory to low (as per the 80x86) or vice versa.
      * portSTACK_GROWTH is used to make the result positive or negative as required
