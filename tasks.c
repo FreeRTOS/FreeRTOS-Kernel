d0c1b5b0e465738d10c5e39c110ff7cead5356e8
Gaurav Aggarwal <aggarg@amazon.com>
2021-12-06 20:56:49 +0000

    Fix CI fomatting check
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- tasks.c
+++ tasks.c
@@ -3529,10 +3529,10 @@ static portTASK_FUNCTION( prvIdleTask, pvParameters )
 
     eSleepModeStatus eTaskConfirmSleepModeStatus( void )
     {
-		#if ( INCLUDE_vTaskSuspend == 1 )
-    		/* The idle task exists in addition to the application tasks. */
-        	const UBaseType_t uxNonApplicationTasks = 1;
-		#endif /* INCLUDE_vTaskSuspend */
+        #if ( INCLUDE_vTaskSuspend == 1 )
+            /* The idle task exists in addition to the application tasks. */
+            const UBaseType_t uxNonApplicationTasks = 1;
+        #endif /* INCLUDE_vTaskSuspend */
 
         eSleepModeStatus eReturn = eStandardSleep;
 
@@ -3554,19 +3554,20 @@ static portTASK_FUNCTION( prvIdleTask, pvParameters )
              * because the scheduler is suspended. */
             eReturn = eAbortSleep;
         }
+
         #if ( INCLUDE_vTaskSuspend == 1 )
-        	else if( listCURRENT_LIST_LENGTH( &xSuspendedTaskList ) == ( uxCurrentNumberOfTasks - uxNonApplicationTasks ) )
-        	{
-        		/* If all the tasks are in the suspended list (which might mean they
-        		 * have an infinite block time rather than actually being suspended)
-        		 * then it is safe to turn all clocks off and just wait for external
-        		 * interrupts. */
+            else if( listCURRENT_LIST_LENGTH( &xSuspendedTaskList ) == ( uxCurrentNumberOfTasks - uxNonApplicationTasks ) )
+            {
+                /* If all the tasks are in the suspended list (which might mean they
+                 * have an infinite block time rather than actually being suspended)
+                 * then it is safe to turn all clocks off and just wait for external
+                 * interrupts. */
                 eReturn = eNoTasksWaitingTimeout;
             }
-		#endif /* INCLUDE_vTaskSuspend */
+        #endif /* INCLUDE_vTaskSuspend */
         else
         {
-                mtCOVERAGE_TEST_MARKER();
+            mtCOVERAGE_TEST_MARKER();
         }
 
         return eReturn;
