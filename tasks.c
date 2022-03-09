9141d8874b91872a2a02da6025d095497fb07736
Darian Leung <darian@espressif.com>
2022-03-09 20:49:47 +0800

    Add port idle hooks
    This commit adds a separate of idle hooks that are exclusive to the port layer
    (i.e., they are not public API). This allows the FreeRTOS ports to run some system
    code from the idle and minimal tasks.

--- tasks.c
+++ tasks.c
@@ -4257,6 +4257,20 @@ void vTaskMissedYield( void )
                 }
             #endif /* ( ( configUSE_PREEMPTION == 1 ) && ( configIDLE_SHOULD_YIELD == 1 ) ) */
 
+            #if ( portUSE_IDLE_HOOK == 1 )
+                {
+                    extern void vPortIdleHook( void );
+
+                    /* Call the port defined function from within the idle task.  This
+                    * allows the port to add background functionality without the
+                    * overhead of a separate task.
+                    *
+                    * NOTE: vPortIdleHook() MUST NOT, UNDER ANY CIRCUMSTANCES,
+                    * CALL A FUNCTION THAT MIGHT BLOCK. */
+                    vPortIdleHook();
+                }
+            #endif
+
             #if ( configUSE_MINIMAL_IDLE_HOOK == 1 )
                 {
                     extern void vApplicationMinimalIdleHook( void );
@@ -4338,6 +4352,20 @@ static portTASK_FUNCTION( prvIdleTask, pvParameters )
                 }
             }
         #endif /* ( ( configUSE_PREEMPTION == 1 ) && ( configIDLE_SHOULD_YIELD == 1 ) ) */
+
+        #if ( portUSE_IDLE_HOOK == 1 )
+            {
+                extern void vPortIdleHook( void );
+
+                /* Call the port defined function from within the idle task.  This
+                 * allows the port to add background functionality without the
+                 * overhead of a separate task.
+                 *
+                 * NOTE: vPortIdleHook() MUST NOT, UNDER ANY CIRCUMSTANCES,
+                 * CALL A FUNCTION THAT MIGHT BLOCK. */
+                vPortIdleHook();
+            }
+        #endif
 
         #if ( configUSE_IDLE_HOOK == 1 )
             {
