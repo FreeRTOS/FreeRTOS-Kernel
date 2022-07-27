cc99f57b4288714c41796e874a5e95b708048f6d
Ching-Hsin Lee <chinglee@amazon.com>
2022-07-27 19:17:21 +0800

    Move suspend scheduler inside critical section

--- tasks.c
+++ tasks.c
@@ -2722,10 +2722,6 @@ BaseType_t xTaskResumeAll( void )
         if( xSchedulerRunning != pdFALSE )
     #endif
     {
-        /* If uxSchedulerSuspended is zero then this function does not match a
-         * previous call to vTaskSuspendAll(). */
-        configASSERT( uxSchedulerSuspended );
-
         /* It is possible that an ISR caused a task to be removed from an event
          * list while the scheduler was suspended.  If this was the case then the
          * removed task will have been added to the xPendingReadyList.  Once the
@@ -2736,6 +2732,10 @@ BaseType_t xTaskResumeAll( void )
             BaseType_t xCoreID;
 
             xCoreID = portGET_CORE_ID();
+
+            /* If uxSchedulerSuspended is zero then this function does not match a
+             * previous call to vTaskSuspendAll(). */
+            configASSERT( uxSchedulerSuspended );
 
             --uxSchedulerSuspended;
             portRELEASE_TASK_LOCK();
