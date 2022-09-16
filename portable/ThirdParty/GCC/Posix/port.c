6f63da20b3c9e1408e7c8007d3427b75878cbd64
aggarg <aggarg@users.noreply.github.com>
2022-09-16 18:40:39 +0000

    [AUTO][RELEASE]: Bump file header version to "10.5.0"

--- portable/ThirdParty/GCC/Posix/port.c
+++ portable/ThirdParty/GCC/Posix/port.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS Kernel <DEVELOPMENT BRANCH>
+ * FreeRTOS Kernel V10.5.0
  * Copyright (C) 2020 Cambridge Consultants Ltd.
  *
  * SPDX-License-Identifier: MIT
--- portable/ThirdParty/GCC/Posix/portmacro.h
+++ portable/ThirdParty/GCC/Posix/portmacro.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS Kernel <DEVELOPMENT BRANCH>
+ * FreeRTOS Kernel V10.5.0
  * Copyright 2020 Cambridge Consultants Ltd.
  *
  * SPDX-License-Identifier: MIT
