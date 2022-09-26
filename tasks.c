41dadcf18131efd49de77786fa50ba6a7c005420
Gaurav Aggarwal <aggarg@amazon.com>
2022-09-26 21:49:41 +0530

    Fix context switch when time slicing is off
    When time slicing is off, context switch should only happen when a
    task with priority higher than the currently executing one is unblocked.
    Earlier the code was invoking a context switch even when a task with
    priority equal the currently executing task was unblocked. This commit
    fixes the code to only do a context switch when a higher priority
    task is unblocked.
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- tasks.c
+++ tasks.c
@@ -2809,9 +2809,13 @@ BaseType_t xTaskIncrementTick( void )
                     {
                         /* Preemption is on, but a context switch should
                          * only be performed if the unblocked task has a
-                         * priority that is equal to or higher than the
-                         * currently executing task. */
-                        if( pxTCB->uxPriority >= pxCurrentTCB->uxPriority )
+                         * priority that is higher than the currently
+                         * executing task.
+                         * The case of equal priority tasks sharing
+                         * processing time (which happens when both
+                         * preemption and time slicing are on) is
+                         * handled below.*/
+                        if( pxTCB->uxPriority > pxCurrentTCB->uxPriority )
                         {
                             xSwitchRequired = pdTRUE;
                         }
