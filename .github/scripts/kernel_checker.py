def7d2df2b0506d3d249334974f51e427c17a41c
Skptak <Skptak@users.noreply.github.com>
2022-11-15 20:10:13 +0000

    [AUTO][RELEASE]: Bump file header version to "10.5.1"

--- .github/scripts/kernel_checker.py
+++ .github/scripts/kernel_checker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #/*
-# * FreeRTOS Kernel <DEVELOPMENT BRANCH>
+# * FreeRTOS Kernel V10.5.1
 # * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
 # *
 # * SPDX-License-Identifier: MIT
@@ -99,7 +99,7 @@ KERNEL_THIRD_PARTY_PATTERNS = [
 
 KERNEL_HEADER = [
     '/*\n',
-    ' * FreeRTOS Kernel <DEVELOPMENT BRANCH>\n',
+    ' * FreeRTOS Kernel V10.5.1\n',
     ' * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.\n',
     ' *\n',
     ' * SPDX-License-Identifier: MIT\n',
