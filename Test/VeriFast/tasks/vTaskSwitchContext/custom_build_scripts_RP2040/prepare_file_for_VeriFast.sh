5e72a831448932551db790ed58ceef13e51c7cdc
Tobias Reinhard <16916681+tobireinhard@users.noreply.github.com>
2022-12-13 08:40:20 -0500

    Deleted deprecated code.

--- Test/VeriFast/tasks/vTaskSwitchContext/custom_build_scripts_RP2040/prepare_file_for_VeriFast.sh
+++ Test/VeriFast/tasks/vTaskSwitchContext/custom_build_scripts_RP2040/prepare_file_for_VeriFast.sh
@@ -30,35 +30,17 @@ echo "$6"
 . "$VF_PROOF_BASE_DIR/paths.sh"
 
 
-VF_PROOF_MOD_SRC_DIR=`vf_proof_mod_src_dir $REPO_BASE_DIR`
-VF_PROOF_MOD_HEADER_DIR=`vf_proof_mod_header_dir $REPO_BASE_DIR`
-PROOF_SETUP_DIR=`vf_proof_setup_dir $REPO_BASE_DIR`
-PROOF_FILES_DIR=`vf_proof_dir $REPO_BASE_DIR`
-PICO_SDK_DIR=`pico_sdk_dir $REPO_BASE_DIR`
-SMP_DEMO_DIR=`smp_demo_dir $REPO_BASE_DIR`
-
-TASKS_C=`vf_annotated_tasks_c $REPO_BASE_DIR`
-
-
 PP_SCRIPT_DIR=`pp_script_dir $REPO_BASE_DIR`
-#BUILD_LOG="$LOG_DIR/build_log--`date +'%y_%m_%d--%H_%M'`.txt"
-TIMESTAMP=`date +'%y_%m_%d--%H_%M'`
 PP_LOG_DIR=`pp_log_dir $REPO_BASE_DIR`
-RW_LOG_DIR=`rw_log_dir $REPO_BASE_DIR`
-LOG_PP_TASK_C="$PP_LOG_DIR/tasks--pp--$TIMESTAMP.c"
-LOG_PP_ERR="$PP_LOG_DIR/error--$TIMESTAMP.c"
-
-LOG_VF_RW_TASK_C="$PP_LOG_DIR/tasks--vf_rw--$TIMESTAMP.c"
-
-PP_OUT_DIR="$VF_PROOF_BASE_DIR/preprocessed_files"
-PP_TASK_C=`pp_vf_tasks_c $REPO_BASE_DIR`
-
-
 FILE_PP_LOG="$PP_LOG_DIR/pp.c"
 FILE_RW_LOG="$PP_LOG_DIR/rw.c"
 
 
-mkdir $PP_LOG_DIR
+# Ensure that log directory exists
+if [ ! -d "$PP_LOG_DIR" ]; then
+    mkdir "$PP_LOG_DIR"
+fi
+
 
 # Preprocessing the source file
 # Output is written to '$FILE_PP_LOG' and error report is written to 
