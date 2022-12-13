3675aa60119313a1a4ca9ca2fde5d6c09b9711d0
Tobias Reinhard <16916681+tobireinhard@users.noreply.github.com>
2022-12-13 10:55:57 -0500

    Deleted deprecated predicates and wrote some documentation.

--- Test/VeriFast/tasks/vTaskSwitchContext/proof/task_predicates.h
+++ Test/VeriFast/tasks/vTaskSwitchContext/proof/task_predicates.h
@@ -7,51 +7,10 @@
 
 /*@
 // This predicate represents the memory corresponding to an
-// uninitialised instance of type `TCB_t` aka `tskTaskControlBlock`.
-predicate uninit_TCB_p(TCB_t * tcb, int stackSize) =
-    malloc_block_tskTaskControlBlock(tcb) &*&
-    tcb->pxTopOfStack |-> _ &*&
-
-    xLIST_ITEM(&tcb->xStateListItem, _, _, _, _, _) &*&
-    struct_xLIST_ITEM_padding(&tcb->xStateListItem) &*&
-    xLIST_ITEM(&tcb->xEventListItem, _, _, _, _, _) &*&
-    struct_xLIST_ITEM_padding(&tcb->xEventListItem) &*&
-    
-    tcb->uxPriority |-> _ &*&
-
-    tcb->pxStack |-> ?stackPtr &*&
-    stackPtr != 0 &*&
-    (char*) stackPtr + stackSize <=  (char*) UINTPTR_MAX &*&
-    chars_((char*) stackPtr, stackSize, _) &*&
-    malloc_block_chars((char*) stackPtr, stackSize) &*&
-
-    tcb->xTaskRunState |-> _ &*&
-    tcb->xIsIdle |-> _ &*&
-    
-    // Assumes macro `configMAX_TASK_NAME_LEN` evaluates to 16.
-    chars_(tcb->pcTaskName, 16, _) &*&
-
-    tcb->uxCriticalNesting |-> _ &*&
-    tcb->uxTCBNumber |-> _ &*&
-    tcb->uxTaskNumber |-> _ &*&
-    tcb->uxBasePriority |-> _ &*&
-    tcb->uxMutexesHeld |-> _ &*&
-
-    // void * pvThreadLocalStoragePointers[ 5 ];
-    pointers_(tcb->pvThreadLocalStoragePointers, 5, _) &*&
-
-    // We assume that the macro `configTASK_NOTIFICATION_ARRAY_ENTRIES`
-    // evaluates to 1.
-    integers__(tcb->ulNotifiedValue, 4, false, 1, _) &*&
-    uchars_((unsigned char*) tcb->ucNotifyState, 1, _) &*&
-
-    tcb->ucDelayAborted |-> _;
-@*/
-
-
-/*@
-// This predicate represents the memory corresponding to an
 // initialised instance of type `TCB_t` aka `tskTaskControlBlock`.
+// The predicate itself is not used during the verification of 
+// `vTaskSwitchContext`. However, we keep it around to allow proof authors to
+// validate that the predicates below indeed capture specific segments of a TCB.
 predicate TCB_p(TCB_t * tcb, uint32_t ulFreeBytesOnStack) =
     malloc_block_tskTaskControlBlock(tcb) &*&
     tcb->pxStack |-> ?stackPtr &*&
@@ -87,10 +46,6 @@ predicate TCB_p(TCB_t * tcb, uint32_t ulFreeBytesOnStack) =
     uchars((unsigned char*) tcb->ucNotifyState, 1, _) &*&
 
     tcb->ucDelayAborted |-> _;
-
-
-predicate absTCB_p(TCB_t* tcb) =
-    TCB_p(tcb, _);
 @*/
 
 /*@ 
@@ -102,12 +57,19 @@ predicate absTCB_p(TCB_t* tcb) =
 //
 // The predicates below will be expanded iteratively.
 
+// This predicate captures the private part of a TCB that should only be
+// accessed by the TCB represents or under specific circumstances where
+// we are certain that the task is not running.
 predicate prvSeg_TCB_p(TCB_t* tcb, uint32_t ulFreeBytesOnStack) =
     tcb->pxStack |-> ?stackPtr &*&
     tcb->pxTopOfStack |-> ?topPtr &*&
     stack_p_2(stackPtr, ?ulStackDepth, topPtr, 
               ulFreeBytesOnStack, ?ulUsedCells, ?ulUnalignedBytes);
 
+// This predicate represents a shared part of a TCB that can be accessed by
+// anyone. Note that this predicate only contains the minimal access rights 
+// required by the `vTaskSwitchContext` proof. It can be extended in the future
+// as needed.
 predicate sharedSeg_TCB_p(TCB_t* tcb, TaskRunning_t state;) =
     tcb->xTaskRunState |-> state;
 
