6d65558ba01141d7c50ff6f93a4054cc5f16940e
tcpluess <tpluess@ieee.org>
2022-12-19 10:48:07 +0100

    move the prototype for vApplicationIdleHook to task.h. (#600)
    Co-authored-by: pluess <pluess@faulhorn.mw.iap.unibe.ch>
    Co-authored-by: Gaurav-Aggarwal-AWS <33462878+aggarg@users.noreply.github.com>

--- tasks.c
+++ tasks.c
@@ -3477,13 +3477,7 @@ static portTASK_FUNCTION( prvIdleTask, pvParameters )
 
         #if ( configUSE_IDLE_HOOK == 1 )
         {
-            extern void vApplicationIdleHook( void );
-
-            /* Call the user defined function from within the idle task.  This
-             * allows the application designer to add background functionality
-             * without the overhead of a separate task.
-             * NOTE: vApplicationIdleHook() MUST NOT, UNDER ANY CIRCUMSTANCES,
-             * CALL A FUNCTION THAT MIGHT BLOCK. */
+            /* Call the user defined function from within the idle task. */
             vApplicationIdleHook();
         }
         #endif /* configUSE_IDLE_HOOK */
