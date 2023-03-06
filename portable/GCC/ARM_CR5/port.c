dd6d45a6a3f9dc8fecee4c99d120b75aa55826b2
Gaurav Aggarwal <aggarg@amazon.com>
2023-03-06 10:19:08 +0000

    Minor code review suggestions
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- portable/GCC/ARM_CR5/port.c
+++ portable/GCC/ARM_CR5/port.c
@@ -76,14 +76,14 @@
 
 /*
  * __ARM_FP is defined by the c preprocessor when FPU support is enabled,
- * ususally with the -mfpu= argument and mfloat-abi=.
+ * usually with the -mfpu= argument and -mfloat-abi=.
  *
  * Note: Some implementations of the c standard library may use FPU registers
  *       for generic memory operations (memcpy, etc).
  *       When setting configUSE_TASK_FPU_SUPPORT == 1, care must be taken to
  *       ensure that the FPU registers are not used without an FPU context.
  */
-#if configUSE_TASK_FPU_SUPPORT == 0
+#if ( configUSE_TASK_FPU_SUPPORT == 0 )
     #ifdef __ARM_FP
         #error __ARM_FP is defined, so configUSE_TASK_FPU_SUPPORT must be set to either to 1 or 2.
     #endif /* __ARM_FP */
@@ -395,11 +395,13 @@ static void prvTaskExitError( void )
 /*-----------------------------------------------------------*/
 
 #if ( configUSE_TASK_FPU_SUPPORT != 0 )
-    void vApplicationFPUSafeIRQHandler( uint32_t ulICCIAR )
+
+    void vApplicationFPUSafeIRQHandler( uint32_t ulICCIAR ) /* __attribute__( ( weak ) ) */
     {
         ( void ) ulICCIAR;
         configASSERT( ( volatile void * ) NULL );
     }
+
 #endif /* configUSE_TASK_FPU_SUPPORT != 0 */
 /*-----------------------------------------------------------*/
 
