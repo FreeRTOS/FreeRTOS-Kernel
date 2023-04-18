3c31ea9d86e6d22e9575d6cc677013c27903f769
Darian <32921628+Dazza0@users.noreply.github.com>
2023-04-18 13:19:57 +0800

    Fix configDEINIT_TLS_BLOCK (#73)
    configDEINIT_TLS_BLOCK() should deinit the TLS block of the task to being
    deleted instead of the currently running task.

--- tasks.c
+++ tasks.c
@@ -5695,7 +5695,7 @@ static void prvCheckTasksWaitingTermination( void )
         #if ( ( configUSE_NEWLIB_REENTRANT == 1 ) || ( configUSE_C_RUNTIME_TLS_SUPPORT == 1 ) )
         {
             /* Free up the memory allocated for the task's TLS Block. */
-            configDEINIT_TLS_BLOCK( pxCurrentTCB->xTLSBlock );
+            configDEINIT_TLS_BLOCK( pxTCB->xTLSBlock );
         }
         #endif
 
