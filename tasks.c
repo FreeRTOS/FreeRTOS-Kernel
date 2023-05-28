b6915be6b045c43216ff56e870ea06ef6910b913
Maria Tomy <62710604+mariatomy9@users.noreply.github.com>
2023-05-28 11:01:58 +0530

    New commt

--- tasks.c
+++ tasks.c
@@ -29,6 +29,7 @@
 /* Standard includes. */
 #include <stdlib.h>
 #include <string.h>
+#include "pico/stdlib.h"
 
 /* Defining MPU_WRAPPERS_INCLUDED_FROM_API_FILE prevents task.h from redefining
  * all the API functions to use the MPU wrappers.  That should only be done when
