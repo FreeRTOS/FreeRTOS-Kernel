9a6284262e583a24633e04d5df5ae71183c29150
chinglee-iot <61685396+chinglee-iot@users.noreply.github.com>
2023-06-21 17:35:18 +0800

    Fix typo in check comment of configMAX_SYSCALL_INTERRUPT_PRIORITY (#690)

--- portable/GCC/ARM_CM23_NTZ/non_secure/port.c
+++ portable/GCC/ARM_CM23_NTZ/non_secure/port.c
@@ -1128,7 +1128,7 @@ BaseType_t xPortStartScheduler( void ) /* PRIVILEGED_FUNCTION */
          * See https://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html */
         configASSERT( ucMaxSysCallPriority );
 
-        /* Check that the bits not implemented bits in hardware are zero in
+        /* Check that the bits not implemented in hardware are zero in
          * configMAX_SYSCALL_INTERRUPT_PRIORITY. */
         configASSERT( ( configMAX_SYSCALL_INTERRUPT_PRIORITY & ( ~ucMaxPriorityValue ) ) == 0U );
 
