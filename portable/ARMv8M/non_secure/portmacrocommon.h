c49a6f9cd474f21533ba006a7f33e8bf01bb0c0c
Rahul Kar <karahulx@amazon.com>
2023-09-07 13:52:59 +0000

    Add ACL changes for GCC/ARMv8M ports

--- portable/ARMv8M/non_secure/portmacrocommon.h
+++ portable/ARMv8M/non_secure/portmacrocommon.h
@@ -287,6 +287,9 @@ extern void vClearInterruptMask( uint32_t ulMask ) /* __attribute__(( naked )) P
     #define portSTACK_FRAME_HAS_PADDING_FLAG    ( 1UL << 0UL )
     #define portTASK_IS_PRIVILEGED_FLAG         ( 1UL << 1UL )
 
+    #define portACL_ENTRY_SIZE_BYTES            ( 4U )
+    #define portACL_ENTRY_SIZE_BITS             ( 32U )
+
     typedef struct MPU_SETTINGS
     {
         uint32_t ulMAIR0;                                              /**< MAIR0 for the task containing attributes for all the 4 per task regions. */
@@ -296,6 +299,9 @@ extern void vClearInterruptMask( uint32_t ulMask ) /* __attribute__(( naked )) P
 
         #if ( configUSE_MPU_WRAPPERS_V1 == 0 )
             xSYSTEM_CALL_STACK_INFO xSystemCallStackInfo;
+            #if ( configENABLE_ACCESS_CONTROL_LIST == 1 )
+                uint32_t ucAccessControlList[ ( configPROTECTED_KERNEL_OBJECT_POOL_SIZE / portACL_ENTRY_SIZE_BYTES ) + 1 ];
+            #endif
         #endif
     } xMPU_SETTINGS;
 
