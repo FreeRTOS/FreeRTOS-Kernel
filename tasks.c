8fbc647ca2bf7ed0657b7e66402e182b7106d051
mehdi-norouzi <mehdeenoroozi@gmail.com>
2023-09-11 08:56:53 +0330

    Fix formatting in tasks.c

--- tasks.c
+++ tasks.c
@@ -7681,41 +7681,41 @@ static void prvAddCurrentTaskToDelayedList( TickType_t xTicksToWait,
 /*-----------------------------------------------------------*/
 
 /*
-  vApplicationGetIdleTaskMemory gets called when configSUPPORT_STATIC_ALLOCATION
-  equals to 1 and is required for static memory allocation support.
-  This definition can be overwritten. 
-*/
+ * vApplicationGetIdleTaskMemory gets called when configSUPPORT_STATIC_ALLOCATION
+ * equals to 1 and is required for static memory allocation support.
+ * This definition can be overwritten.
+ */
 
-#if (configSUPPORT_STATIC_ALLOCATION == 1)
+#if ( configSUPPORT_STATIC_ALLOCATION == 1 )
 
-__attribute__((__weak__)) void vApplicationGetIdleTaskMemory (StaticTask_t **ppxIdleTaskTCBBuffer,
-                                                             StackType_t **ppxIdleTaskStackBuffer,
-                                                             uint32_t *pulIdleTaskStackSize) 
-{
-    /* Idle task control block and stack */
-    static StaticTask_t Idle_TCB;
-    static StackType_t  Idle_Stack[configMINIMAL_STACK_SIZE];
+    __attribute__( ( __weak__ ) ) void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
+                                                                      StackType_t ** ppxIdleTaskStackBuffer,
+                                                                      uint32_t * pulIdleTaskStackSize )
+    {
+        /* Idle task control block and stack */
+        static StaticTask_t Idle_TCB;
+        static StackType_t Idle_Stack[ configMINIMAL_STACK_SIZE ];
 
-    *ppxIdleTaskTCBBuffer   = &Idle_TCB;
-    *ppxIdleTaskStackBuffer = &Idle_Stack[0];
-    *pulIdleTaskStackSize   = (uint32_t)configMINIMAL_STACK_SIZE;
-}
+        *ppxIdleTaskTCBBuffer = &Idle_TCB;
+        *ppxIdleTaskStackBuffer = &Idle_Stack[ 0 ];
+        *pulIdleTaskStackSize = ( uint32_t ) configMINIMAL_STACK_SIZE;
+    }
 
 /*
-  vApplicationGetTimerTaskMemory gets called when configSUPPORT_STATIC_ALLOCATION
-  equals to 1 and is required for static memory allocation support.
-  This definition can be overwritten. 
-*/
-__attribute__((__weak__)) void vApplicationGetTimerTaskMemory (StaticTask_t **ppxTimerTaskTCBBuffer,
-                                                               StackType_t **ppxTimerTaskStackBuffer,
-                                                               uint32_t *pulTimerTaskStackSize) 
-{
-    /* Timer task control block and stack */
-    static StaticTask_t Timer_TCB;
-    static StackType_t  Timer_Stack[configTIMER_TASK_STACK_DEPTH];
+ * vApplicationGetTimerTaskMemory gets called when configSUPPORT_STATIC_ALLOCATION
+ * equals to 1 and is required for static memory allocation support.
+ * This definition can be overwritten.
+ */
+    __attribute__( ( __weak__ ) ) void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
+                                                                       StackType_t ** ppxTimerTaskStackBuffer,
+                                                                       uint32_t * pulTimerTaskStackSize )
+    {
+        /* Timer task control block and stack */
+        static StaticTask_t Timer_TCB;
+        static StackType_t Timer_Stack[ configTIMER_TASK_STACK_DEPTH ];
 
-    *ppxTimerTaskTCBBuffer   = &Timer_TCB;
-    *ppxTimerTaskStackBuffer = &Timer_Stack[0];
-    *pulTimerTaskStackSize   = (uint32_t)configTIMER_TASK_STACK_DEPTH;
-}
-#endif
+        *ppxTimerTaskTCBBuffer = &Timer_TCB;
+        *ppxTimerTaskStackBuffer = &Timer_Stack[ 0 ];
+        *pulTimerTaskStackSize = ( uint32_t ) configTIMER_TASK_STACK_DEPTH;
+    }
+#endif /* if ( configSUPPORT_STATIC_ALLOCATION == 1 ) */
