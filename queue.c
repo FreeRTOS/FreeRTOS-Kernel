8e3481cb9c84d50b77e21703088a07776de0628b
Ching-Hsin Lee <chinglee@amazon.com>
2023-10-05 16:02:52 +0800

    Revert all the change in queue.c

--- queue.c
+++ queue.c
@@ -48,6 +48,7 @@
  * correct privileged Vs unprivileged linkage and placement. */
 #undef MPU_WRAPPERS_INCLUDED_FROM_API_FILE /*lint !e961 !e750 !e9021. */
 
+
 /* Constants used with the cRxLock and cTxLock structure members. */
 #define queueUNLOCKED             ( ( int8_t ) -1 )
 #define queueLOCKED_UNMODIFIED    ( ( int8_t ) 0 )
