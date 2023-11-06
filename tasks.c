bf35fc4b49011572f4a96d078151b252f0ef5332
Moral-Hao <sihunzhilei@126.com>
2023-11-06 02:19:53 +0000

    Add missing definition of variable x.

--- tasks.c
+++ tasks.c
@@ -6157,6 +6157,8 @@ static void prvCheckTasksWaitingTermination( void )
                             }
                             else
                             {
+                                BaseType_t x;
+                                
                                 /* The task does not appear on the event list item of
                                  * and of the RTOS objects, but could still be in the
                                  * blocked state if it is waiting on its notification
