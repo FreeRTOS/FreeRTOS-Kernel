ae1fd1f275b07a23b9c8e77cdc137a5b06f454a4
Soren Ptak <ptaksoren@gmail.com>
2023-11-28 08:57:37 -0500

    Rely upon the WIN32_LEAN_AND_MEAN define to include winsock2.h, revert changes in port.c

--- portable/MSVC-MingW/portmacro.h
+++ portable/MSVC-MingW/portmacro.h
@@ -31,16 +31,12 @@
 
 /** Exclude Windows APIs such as Cryptography, DDE, RPC, Shell,
  * and Windows Sockets. */
-#ifndef WIN32_LEAN_AND_MEAN
-    #define WIN32_LEAN_AND_MEAN
+#ifdef WIN32_LEAN_AND_MEAN
+    #include <winsock2.h>
+#else
+    #include <winsock.h>
 #endif
 
-#include <winsock2.h>
-
-/* Define _WINSOCKAPI_ to ensure that winsock.h is not included */
-#ifndef _WINSOCKAPI_
-    #define _WINSOCKAPI_
-#endif
 #include <windows.h>
 #include <timeapi.h>
 #include <mmsystem.h>
