3e6ff8e6591d280c4625c3b5396748f44b316891
kar-rahul-aws <karahulx@amazon.com>
2024-01-31 11:59:45 +0530

    Update formatting

--- examples/template_configuration/FreeRTOSConfig.h
+++ examples/template_configuration/FreeRTOSConfig.h
@@ -561,7 +561,7 @@
  * and Cortex-M85 ports as M-Profile Vector Extension (MVE) is available only on
  * these architectures. configENABLE_MVE must be left undefined, or defined to 0
  * for the Cortex-M23,Cortex-M33 and Cortex-M35P ports. */
-#define configENABLE_MVE                      1
+#define configENABLE_MVE                  1
 
 /******************************************************************************/
 /* Definitions that include or exclude functionality. *************************/
