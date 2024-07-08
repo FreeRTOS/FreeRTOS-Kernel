d37b60563263c8e5aad6d7ffae33f766a120d428
kar-rahul-aws <karahulx@amazon.com>
2024-07-08 14:42:46 +0530

    First update of vPortTaskUsesFPU API

--- portable/GCC/ARM_CRx_No_GIC/port.c
+++ portable/GCC/ARM_CRx_No_GIC/port.c
@@ -312,13 +312,14 @@ void FreeRTOS_Tick_Handler( void )
 
 void vPortTaskUsesFPU( void )
 {
-    uint32_t ulInitialFPSCR = 0;
+    //uint32_t ulInitialFPSCR = 0;
 
     /* A task is registering the fact that it needs an FPU context.  Set the
      * FPU flag (which is saved as part of the task context). */
     ulPortTaskHasFPUContext = pdTRUE;
 
     /* Initialise the floating point status register. */
-    __asm volatile ( "FMXR  FPSCR, %0" ::"r" ( ulInitialFPSCR ) : "memory" );
+    vSetupFPU();
+    /*__asm volatile ( "FMXR  FPSCR, %0" ::"r" ( ulInitialFPSCR ) : "memory" );*/
 }
 /*-----------------------------------------------------------*/
