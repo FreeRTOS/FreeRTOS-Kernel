5d1d9dfda89c4137e13695d9b9fc1795507140de
Gaurav Aggarwal <aggarg@amazon.com>
2024-09-29 09:44:21 +0000

    Change the cast from UBaseType_t to size_t
    This was causing problem for 8-bit ports.
    
    This was reported here - https://github.com/feilipu/Arduino_FreeRTOS_Library/issues/136
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- queue.c
+++ queue.c
@@ -513,7 +513,7 @@ BaseType_t xQueueGenericReset( QueueHandle_t xQueue,
             /* Check for multiplication overflow. */
             ( ( SIZE_MAX / uxQueueLength ) >= uxItemSize ) &&
             /* Check for addition overflow. */
-            ( ( UBaseType_t ) ( SIZE_MAX - sizeof( Queue_t ) ) >= ( uxQueueLength * uxItemSize ) ) )
+            ( ( SIZE_MAX - sizeof( Queue_t ) ) >= ( size_t ) ( uxQueueLength * uxItemSize ) ) )
         {
             /* Allocate enough space to hold the maximum number of items that
              * can be in the queue at any time.  It is valid for uxItemSize to be
