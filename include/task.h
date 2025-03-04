82c897ae30c5258087b16c973fb1e6800856ea92
rohitmadan07 <rohitmadan07@users.noreply.github.com>
2025-03-04 08:34:17 +0000

    [AUTO][RELEASE]: Bump task.h version macros to "11.2.0"

--- include/task.h
+++ include/task.h
@@ -53,9 +53,9 @@
  * The tskKERNEL_VERSION_MAJOR, tskKERNEL_VERSION_MINOR, tskKERNEL_VERSION_BUILD
  * values will reflect the last released version number.
  */
-#define tskKERNEL_VERSION_NUMBER                      "V11.1.0+"
+#define tskKERNEL_VERSION_NUMBER                      "V11.2.0"
 #define tskKERNEL_VERSION_MAJOR                       11
-#define tskKERNEL_VERSION_MINOR                       1
+#define tskKERNEL_VERSION_MINOR                       2
 #define tskKERNEL_VERSION_BUILD                       0
 
 /* MPU region parameters passed in ulParameters
