5e7aa6e3d42f3bbe2f0b84405ef55ec31faf5371
Gaurav Aggarwal <aggarg@amazon.com>
2025-04-25 11:14:24 +0000

    Fix formatting check
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- include/croutine.h
+++ include/croutine.h
@@ -246,7 +246,10 @@ void vCoRoutineSchedule( void );
  * \defgroup crSTART crSTART
  * \ingroup Tasks
  */
+
+/* *INDENT-OFF* */
 #define crEND()    }
+/* *INDENT-ON* */
 
 /*
  * These macros are intended for internal use by the co-routine implementation
