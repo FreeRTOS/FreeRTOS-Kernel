73be3753a8b3092cb1e2fb204f7d67718f9b197f
Gaurav Aggarwal <aggarg@amazon.com>
2025-05-02 04:23:42 +0000

    Revert "FreeRTOS SMP: direct access to current TCB inside stack macros (#1270)"
    This reverts commit 4162ca49d6aa1bdef940e09b71c28b51779db523.

--- tasks.c
+++ tasks.c
@@ -5251,7 +5251,7 @@ BaseType_t xTaskIncrementTick( void )
                 #endif /* configGENERATE_RUN_TIME_STATS */
 
                 /* Check for stack overflow, if configured. */
-                taskCHECK_FOR_STACK_OVERFLOW( xCoreID );
+                taskCHECK_FOR_STACK_OVERFLOW();
 
                 /* Before the currently running task is switched out, save its errno. */
                 #if ( configUSE_POSIX_ERRNO == 1 )
