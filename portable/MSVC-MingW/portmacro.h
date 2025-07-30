b27722a30e86eeec62fd85daac488362547a02c7
Christoph Reiter <christoph.reiter@evk.biz>
2025-07-30 16:06:30 +0200

    [MSVC-MingW] Move windows headers to port.c
    This prevents the inclusion of windows.h. into all header files using
    FreeRTOS.h and thus defining several macros conflicting with common
    definitions.

--- portable/MSVC-MingW/portmacro.h
+++ portable/MSVC-MingW/portmacro.h
@@ -29,17 +29,6 @@
 #ifndef PORTMACRO_H
 #define PORTMACRO_H
 
-#ifdef WIN32_LEAN_AND_MEAN
-    #include <winsock2.h>
-#else
-    #include <winsock.h>
-#endif
-
-#include <windows.h>
-#include <timeapi.h>
-#include <mmsystem.h>
-#include <winbase.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
